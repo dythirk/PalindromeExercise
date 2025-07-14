21c717bed133b23e07b8b1a2e124215c48e5612c
Thai Vo <tahrbo@gmail.com>
2025-07-13 23:04:01 -0500

    ready

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string phrase)
+        {
+            List<char> words = phrase.ToList();
+
+            int tally = 0;
+            string reverse = "";
+
+            if (phrase == null)
+            {
+                return false;
+            }
+
+            for (int i = words.Count-1; i >= 0; i--) {
+                reverse += words[i].ToString();
+            }
+
+            if (reverse == phrase)
+            {
+                return true;
+            }
+            else
+            { 
+                return false;
+            }
+        }
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+﻿using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,27 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("a", true)]
+        [InlineData("abc", false)]
+        [InlineData("1", true)]
+        [InlineData("12", false)]
+        [InlineData("121", true)]
+        [InlineData("face", false)]
+        [InlineData("racecar", true)]
+        [InlineData("Racecar", false)]
+        [InlineData("Panama", false)]
+        [InlineData("amanaplanacanalpanama", true)]
+        public void IsAPalindromeTest(string phrase, bool expected)
         {
+            //Arrange
+            var wordSmithTestClass = new WordSmith();
 
+            //Act
+            var actual = wordSmithTestClass.IsAPalindrome(phrase);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
